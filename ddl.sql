94c847be157c7675f4d0fec963fd91eb93a74a48
Víctor Pérez <vmpch1@outlook.com>
2025-04-12 21:53:18 -0600

    DAPA-34 | fix: change password length

--- ddl.sql
+++ ddl.sql
@@ -50,7 +50,7 @@ CREATE TABLE users (
 CREATE TABLE employees (
     id SERIAL PRIMARY KEY,
 	user_id INTEGER NOT NULL,
-    password VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     role VARCHAR(20) NOT NULL,
 	FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
