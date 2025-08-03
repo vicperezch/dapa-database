a30c20c119e3e2bb024f15111a2e103e12cdcd9c
Víctor Pérez <vmpch1@outlook.com>
2025-08-03 01:58:26 -0600

    DAPA-139 | chore: refactor tables

--- setup/001-ddl.sql
+++ setup/001-ddl.sql
@@ -1,23 +1,9 @@
 CREATE type form_status AS ENUM('pending', 'cancelled', 'approved');
+CREATE type role AS ENUM('admin', 'driver');
 
-CREATE TABLE service_type (
+CREATE TABLE service_types (
     id SERIAL PRIMARY KEY,
     description VARCHAR(25)
-);
-
-CREATE TABLE payment_method (
-    id SERIAL PRIMARY KEY,
-    description VARCHAR(50) NOT NULL
-);
-
-CREATE TABLE payment_indicator (
-    id SERIAL PRIMARY KEY,
-    description VARCHAR(25) NOT NULL
-);
-
-CREATE TABLE expense_type (
-    id SERIAL PRIMARY KEY,
-    description VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE vehicles (
@@ -27,18 +13,12 @@ CREATE TABLE vehicles (
     license_plate VARCHAR(15) NOT NULL,
     capacity_kg DECIMAL(10,2) CHECK (capacity_kg > 0),
     available BOOLEAN,
-    current_mileage DECIMAL(10,2) NOT NULL CHECK (current_mileage > 0),
-    next_maintenance_mileage DECIMAL(10,2) NOT NULL CHECK (next_maintenance_mileage > 0)
+	insurance DATE NOT NULL DEFAULT CURRENT_DATE
 );
 
-CREATE TABLE states (
+CREATE TABLE statuses (
     id SERIAL PRIMARY KEY,
-    description VARCHAR NOT NULL
-);
-
-CREATE TABLE client_addresses (
-    id SERIAL PRIMARY KEY,
-    address VARCHAR(100) NOT NULL
+    description VARCHAR(25) NOT NULL
 );
 
 CREATE TABLE users (
@@ -46,68 +26,10 @@ CREATE TABLE users (
     name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     phone VARCHAR(20) NOT NULL,
-    email VARCHAR(50) NOT NULL UNIQUE DEFAULT 'unknown'
-);
-
-CREATE TABLE employees (
-    id SERIAL PRIMARY KEY,
-	user_id INTEGER NOT NULL,
-	license_expiration_date DATE,
-    password VARCHAR(255) NOT NULL,
-    role VARCHAR(20) NOT NULL,
-	FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
-
-CREATE TABLE financial_reports (
-    id SERIAL PRIMARY KEY,
-    date DATE NOT NULL,
-    total_income DECIMAL(10,2),
-    total_expense DECIMAL(10,2),
-    net_profit DECIMAL(10,2)
-);
-
-CREATE TABLE incomes (
-    id SERIAL PRIMARY KEY,
-    justification TEXT,
-    payment_method_id INTEGER NOT NULL,
-    date DATE NOT NULL DEFAULT CURRENT_DATE,
-    amount DECIMAL(10,2) CHECK (amount > 0),
-    financial_report_id INTEGER NOT NULL,
-    FOREIGN KEY (payment_method_id) REFERENCES payment_method(id),
-    FOREIGN KEY (financial_report_id) REFERENCES financial_reports(id)
-);
-
-CREATE TABLE expenses (
-    id SERIAL PRIMARY KEY,
-    details TEXT,
-    payment_method_id INTEGER NOT NULL,
-    date DATE NOT NULL DEFAULT CURRENT_DATE,
-    payment_indicator_id INTEGER NOT NULL,
-    financial_report_id INTEGER NOT NULL,
-    amount DECIMAL(10,2) CHECK (amount > 0),
-    type_id INTEGER,
-    FOREIGN KEY (payment_method_id) REFERENCES payment_method(id),
-    FOREIGN KEY (payment_indicator_id) REFERENCES payment_indicator(id),
-    FOREIGN KEY (financial_report_id) REFERENCES financial_reports(id),
-    FOREIGN KEY (type_id) REFERENCES expense_type(id) ON DELETE RESTRICT
-);
-
-CREATE TABLE performance_reports (
-    id SERIAL PRIMARY KEY,
-    date DATE NOT NULL,
-    driver_id INTEGER NOT NULL,
-    completed_deliveries INTEGER NOT NULL,
-    average_delivery_time INTEGER NOT NULL,
-    average_rating DECIMAL(10,2),
-    FOREIGN KEY (driver_id) REFERENCES employees(id)
-);
-
-CREATE TABLE notifications (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,
-    message TEXT NOT NULL,
-    date TIMESTAMP NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    email VARCHAR(50) NOT NULL UNIQUE,
+	password_hash VARCHAR(255) NOT NULL,
+	role role NOT NULL,
+	license_expiration_date DATE
 );
 
 CREATE TABLE question_types (
@@ -133,10 +55,8 @@ CREATE TABLE question_options (
 
 CREATE TABLE submissions (
     id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL,
     submitted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	status form_status NOT NULL DEFAULT 'pending',
-    FOREIGN KEY (user_id) REFERENCES users(id)
+	status form_status NOT NULL DEFAULT 'pending'
 );
 
 CREATE TABLE answers (
@@ -150,43 +70,20 @@ CREATE TABLE answers (
 	FOREIGN KEY (option_id) REFERENCES question_options(id)
 );
 
-CREATE TABLE quotes (
-    id SERIAL PRIMARY KEY,
-	submission_id INTEGER NOT NULL,
-	driver_id INTEGER,
-	vehicle_id INTEGER,
-    state_id INTEGER NOT NULL,
-    service_type INTEGER NOT NULL,
-    total_amount DECIMAL(10,2) NOT NULL,
-    date DATE NOT NULL DEFAULT CURRENT_DATE,
-    details TEXT,
-    FOREIGN KEY (state_id) REFERENCES states(id),
-	FOREIGN KEY (submission_id) REFERENCES submissions(id),
-	FOREIGN KEY (driver_id) REFERENCES employees(id),
-	FOREIGN KEY (vehicle_id) REFERENCES vehicles(id)
-);
-
-CREATE TABLE products (
-    id SERIAL PRIMARY KEY,
-    description TEXT,
-    weight_kg DECIMAL(10,2) CHECK (weight_kg > 0),
-    dimensions DECIMAL(10,2),
-    is_fragile BOOLEAN NOT NULL,
-    quote_id INTEGER NOT NULL,
-    FOREIGN KEY (quote_id) REFERENCES quotes(id)
-);
-
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
-    quote_id INTEGER NOT NULL,
-    driver_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
     vehicle_id INTEGER NOT NULL,
     origin VARCHAR(100) NOT NULL,
     destination VARCHAR(100) NOT NULL,
-    state_id INTEGER NOT NULL,
-    FOREIGN KEY (quote_id) REFERENCES quotes(id),
-    FOREIGN KEY (driver_id) REFERENCES employees(id),
+	total_amount DECIMAL(10, 2) NOT NULL,
+    status_id INTEGER NOT NULL,
+	details TEXT,
+	service_type_id INTEGER NOT NULL,
+	date DATE NOT NULL DEFAULT CURRENT_DATE,
+    FOREIGN KEY (user_id) REFERENCES users(id),
     FOREIGN KEY (vehicle_id) REFERENCES vehicles(id),
-    FOREIGN KEY (state_id) REFERENCES states(id)
+    FOREIGN KEY (status_id) REFERENCES statuses(id),
+    FOREIGN KEY (service_type_id) REFERENCES service_types(id)
 );
 
